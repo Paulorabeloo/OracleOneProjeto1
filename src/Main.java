be5ac92b29d2206b9466853b987f3528e59fa279
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2025-04-15 08:43:51 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento); //concatenação
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.0;
+    }
+}
