b186a7d0dc254a9800b9b0444fbdddab78292696
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2025-04-15 09:14:33 -0300

    Atualização

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,21 @@ public class Main {
         System.out.println("Filme: Top Gun: Maverick");
 
         int anoDeLancamento = 2022;
-        System.out.println("Ano de lançamento: " + anoDeLancamento); //concatenação
+        System.out.println("Ano de lançamento: " + anoDeLancamento); // concatenação
         boolean incluidoNoPlano = false;
-        double notaDoFilme = 8.0;
+        double notaDoFilme = 8.1;
+
+        // Média calculada pelas 3 notas da Jack, Paulo e Suelen
+        double media = (9.8 + 6.3 + 8) / 3;
+        System.out.println(media);
+        String sinopse; // string é uma classe
+        // assim é bom para mostrar várias coisas de uma vez como texto
+        sinopse = """ 
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
     }
 }
