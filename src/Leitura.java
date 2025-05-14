05847b589cfe5a89d0b2b4d5deefacac5ed0d31a
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2025-05-14 09:02:18 -0300

    Scanner

--- src/Leitura.java
+++ src/Leitura.java
@@ -1,10 +1,13 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
+//classe para ler dados
 public class Leitura {
     public static void main(String[] args) {
+        // instância da classe scanner passando o objeto System.in como parâmetro para ler a entrada do usuário pelo teclado.
         Scanner leitura = new Scanner(System.in);
 
+        // métodos para ler nextLine(),nextInt(), de acordo com o tipo
         System.out.println("Digite seu filme favorito");
         String filme = leitura.nextLine();
 
