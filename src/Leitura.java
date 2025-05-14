d75798eae8ca1b962b9e17c9b50087f2e9471c4b
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2025-05-14 08:51:53 -0300

    Scanner

---
+++ src/Leitura.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito");
+        String filme = leitura.nextLine();
+
+        System.out.println("Qual o ano de lançamento?");
+        int anoDeLancamento = leitura.nextInt();
+
+        System.out.println("Qual avaliação você da para o filme?");
+        Double nota = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(nota);
+
+
+    }
+}
